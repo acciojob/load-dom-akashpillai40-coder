d52aa07d7dd6d502192c0368643a9893143866e8
Akash <akashpillai40@gmail.com>
2026-04-18 20:10:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-	let string = document.createElement("div");
-string.textContent = "DOM load success";
-document.body.appendChild(string);
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+	document.body.textContent = "DOM load success";
 });
